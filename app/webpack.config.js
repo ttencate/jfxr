4f5e4a79c178f9856c89c1df24d649972a37ee87
Thomas ten Cate <ttencate@gmail.com>
2018-10-22 09:50:38 +0200

    Fix file reference
    Hasty refactorings are never good.
    
    Fixes #32

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './app/js/index.js',
+  entry: './js/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
